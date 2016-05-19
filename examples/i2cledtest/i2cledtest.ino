9ce00d770e67c682ca1c239b6569f8e58b0042ae
Evan Simkowitz <Evan.Simkowitz@studentpartner.com>
2016-05-19 18:37:01 -0400

    added examples

---
+++ examples/i2cledtest/i2cledtest.ino
@@ -0,0 +1,36 @@
+/* 
+ *  ledtest.ino
+ *  
+ *  Copyright (c) 2016, Evan Simkowitz
+ *  Wiring for the chip can be found at:
+ *  http://gtbtech.com/?p=875
+ *  Wire an led for the GP0 pin using the setup suggested in the wiring
+ *  diagram above.
+ */
+#include "Adafruit_MCP23008/Adafruit_MCP23008.h"
+
+bool ledOn = false;
+
+unsigned long int ledAccumulator = 0;
+const unsigned int ledDelay = 1000;
+
+Adafruit_MCP23008 mcp;
+void setup() {
+    mcp.begin();
+    mcp.pinMode(0, OUTPUT);
+    mcp.digitalWrite(0, LOW);
+}
+
+void loop() {
+    if (millis() - ledAccumulator > ledDelay) {
+        ledAccumulator = millis();
+        if (ledOn) {
+            mcp.digitalWrite(0, LOW);
+            ledOn = false;
+        }
+        else {
+            mcp.digitalWrite(0, HIGH);
+            ledOn = true;
+        }
+    }
+}
